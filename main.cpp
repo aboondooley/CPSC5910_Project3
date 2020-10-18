8746ae8d8404ef0fe0cf20914c5f149b8fa371ce
Alie <aboondooley@gmail.com>
2020-10-17 19:56:35 -0700

    Updated tests to test traversals.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "BST.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    BST bst;
+    cout << "Height: " << bst.getHeight() << endl;
+    cout << "Size: " << bst.size() << endl;
+    cout << "Leaves: " << bst.getLeafCount() << endl << endl;
+    cout << bst.getInOrderTraversal() << endl << endl;
+    cout << bst.getPreOrderTraversal() << endl << endl;
+    cout << bst.getPostOrderTraversal() << endl << endl;
+
+    int input[] = {40, 20, 10, 30, 60, 50, 70};
+    for (int i = 0; i < 7; i++)
+        bst.add(input[i]);
+    cout << "Height: " << bst.getHeight() << endl;
+    cout << "Size: " << bst.size() << endl;
+    cout << "Leaves: " << bst.getLeafCount() << endl << endl;
+    cout << bst.getInOrderTraversal() << endl << endl;
+    cout << bst.getPreOrderTraversal() << endl << endl;
+    cout << bst.getPostOrderTraversal() << endl << endl;
     return 0;
 }
